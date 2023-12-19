3b32c0c7619f638903c09b912619835fa1679818
5TAHUR5 <78656900+5TAHUR5@users.noreply.github.com>
2023-12-19 18:47:17 +0500

    Update titlebars.lua

--- awesome/ui/titlebars.lua
+++ awesome/ui/titlebars.lua
@@ -6,103 +6,7 @@ local dpi = require('beautiful').xresources.apply_dpi
 
 -- titlebars --
 
-local set_icon_for_btn = function(btn, value, icon_set_to_on, icon_set_to_off)
-	if value == false then 
-		btn.widget.text = icon_set_to_on
-	else
-		btn.widget.text = icon_set_to_off
-	end
-end
-
-
-local create_button = function(color, name, icon)
-	local widget =  wibox.widget {
-		widget = wibox.container.background,
-		forced_width = dpi(22),
-		forced_height = dpi(22),
-		bg = color,
-		fg = beautiful.background_accent,
-		-- buttons = {
-		-- 	awful.button({}, 1, function()
-		-- 		func()
-		-- 	end)
-		-- },
-		{
-			widget = wibox.widget.textbox,
-			text = icon,
-			font = beautiful.font .. " 13",
-			halign = "center",
-    		valign = "center",
-		}
-	}
-	local popup = awful.tooltip {
-		objects = { widget },
-		timer_function = function()
-			return name
-		end,
-		gaps = beautiful.useless_gap,
-		margins_leftright = dpi(10),
-		margins_topbottom = dpi(10),
-		delay_show = 1.5
-	}
-	return widget
-end
-
 client.connect_signal("request::titlebars", function(c)
-	local minimize = create_button(beautiful.yellow, "Minimize", "—")
-	minimize:buttons {
-		awful.button({}, 1, function()
-			gears.timer.delayed_call(function()
-					c.minimized = not c.minimized
-				end)
-		end)
-	}
-
-	local maximize = create_button(beautiful.green, "Maximize", " 󰊓 ")
-	set_icon_for_btn(maximize, c.maximized, " 󰊓 ", " 󰊔 ")
-	maximize:buttons {
-		awful.button({}, 1, function()
-			c.maximized = not c.maximized
-			set_icon_for_btn(maximize, c.maximized, " 󰊓 ", " 󰊔 ")
-		end)
-	}
-
-	local ontop = create_button(beautiful.violet, "OnTop", "󰸇")
-	set_icon_for_btn(ontop, c.ontop, "󰸇", "󰇭 ")
-	ontop:buttons {
-		awful.button({}, 1, function()
-			c.ontop = not c.ontop
-			set_icon_for_btn(ontop, c.ontop, "󰸇", "󰇭 ")
-		end)
-	}
-
-	local sticky = create_button(beautiful.blue, "Sticky", "󰤱")
-	set_icon_for_btn(sticky, c.sticky, "󰤱", "󰤰 ")
-
-	sticky:buttons {
-		awful.button({}, 1, function()
-			c.sticky = not c.sticky
-			set_icon_for_btn(sticky, c.sticky, "󰤱", "󰤰 ")
-		end)
-	}
-
-	local floating = create_button(beautiful.orange, "FloatingOn", "󰪏 ")
-	set_icon_for_btn(floating, c.floating, "󰪏 ", " ")
-	floating:buttons {
-		awful.button({}, 1, function()
-			c.floating = not c.floating
-			set_icon_for_btn(floating, c.floating, "󰪏 ", " ")
-		end)
-	}
-
-	
-	local close = create_button(beautiful.red, "Kill", "󰖭")
-	close:buttons {
-		awful.button({ }, 1, function()
-			c:kill()
-		end)
-	}
-
 
 	local buttons = gears.table.join(
 		awful.button({ }, 1, function()
@@ -175,11 +79,11 @@ client.connect_signal("request::titlebars", function(c)
 				{
 					layout = wibox.layout.fixed.vertical,
 					spacing = dpi(6),
-					sticky,
-					floating,
-					ontop,
-					minimize,
-					maximize,
+					awful.titlebar.widget.stickybutton(c),
+					awful.titlebar.widget.floatingbutton(c),
+					awful.titlebar.widget.ontopbutton(c),
+					awful.titlebar.widget.minimizebutton(c),
+					awful.titlebar.widget.maximizedbutton(c),
 					awful.titlebar.widget.closebutton(c),
 				}
 			}
@@ -211,11 +115,11 @@ client.connect_signal("request::titlebars", function(c)
 				{
 					layout = wibox.layout.fixed.horizontal,
 					spacing = dpi(6),
-					sticky,
-					floating,
-					ontop,
-					minimize,
-					maximize,
+					awful.titlebar.widget.stickybutton(c),
+					awful.titlebar.widget.floatingbutton(c),
+					awful.titlebar.widget.ontopbutton(c),
+					awful.titlebar.widget.minimizebutton(c),
+					awful.titlebar.widget.maximizedbutton(c),
 					awful.titlebar.widget.closebutton(c),
 				}
 			}
@@ -238,11 +142,11 @@ client.connect_signal("request::titlebars", function(c)
 					layout = wibox.layout.fixed.horizontal,
 					spacing = dpi(6),
 					awful.titlebar.widget.closebutton(c),
-					maximize,
-					minimize,
-					ontop,
-					floating,
-					sticky
+					awful.titlebar.widget.maximizedbutton(c),
+					awful.titlebar.widget.minimizebutton(c),
+					awful.titlebar.widget.ontopbutton(c),
+					awful.titlebar.widget.floatingbutton(c),
+					awful.titlebar.widget.stickybutton(c),
 				}
 			}
 		},
